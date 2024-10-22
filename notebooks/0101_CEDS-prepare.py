1d137e066383f3af63c02c274716dbf51f62f055
Jarmo Kikstra <46967503+jkikstra@users.noreply.github.com>
2024-10-22 14:26:40 +0200

    Revert "Move local package into local package"

--- notebooks/0101_CEDS-prepare.py
+++ notebooks/0101_CEDS-prepare.py
@@ -19,6 +19,11 @@ import pandas as pd
 import pandas_indexing as pix
 from pandas_indexing.core import isna
 
+# import internal functions
+import os
+import sys
+repo_root = os.path.abspath(os.path.join(os.getcwd(), '..')) # the notebook is in a 'notebooks' folder, so we want to go up one level to the root
+sys.path.append(repo_root) # Append the repository root to sys.path
 from emissions_harmonization_historical.ceds import get_map, read_CEDS, add_global
 
 # set unit registry
@@ -36,18 +41,16 @@ ceds_processed_output_file = Path("..", "data", "national", "ceds", "processed",
 # Specify gases to processes
 
 # use all gases covered in CEDS
-gases = [
-    "BC",
-    "CH4",
-    "CO",
-    "CO2",
-    "N2O",  # new, to have regional, was global in CMIP6
-    "NH3",
-    "NMVOC",  # assumed to be equivalent to IAMC-style reported VOC
-    "NOx",
-    "OC",
-    "SO2",
-]
+gases = ["BC",
+         "CH4",
+         "CO",
+         "CO2",
+         "N2O", # new, to have regional, was global in CMIP6
+         "NH3",
+         "NMVOC", # assumed to be equivalent to IAMC-style reported VOC
+         "NOx",
+         "OC",
+         "SO2"]
 
 # Load sector mapping of emissions species
 
@@ -58,20 +61,26 @@ ceds_map.to_frame(index=False)
 # Read CEDS emissions data
 
 ceds = pd.concat(
-    read_CEDS(Path(ceds_data_folder) / f"{gas}_CEDS_emissions_by_country_sector_v{ceds_release}.csv") for gas in gases
+    read_CEDS(
+        Path(ceds_data_folder)
+        / f"{gas}_CEDS_emissions_by_country_sector_v{ceds_release}.csv"
+    )
+    for gas in gases
 ).rename_axis(index={"region": "country"})
 ceds.attrs["name"] = "CEDS21"
 ceds = ceds.pix.semijoin(ceds_map, how="outer")
-ceds.loc[isna].pix.unique(["sector_59", "sector"])  # print sectors with NAs
+ceds.loc[isna].pix.unique(["sector_59", "sector"]) # print sectors with NAs
 
-ceds = ceds.pix.dropna(subset=["units"]).pix.format(unit="{units}/yr", drop=True)  # adjust units
-ceds = pix.units.convert_unit(ceds, lambda x: "Mt " + x.removeprefix("kt").strip())  # adjust units
+ceds = ceds.pix.dropna(subset=["units"]).pix.format(unit="{units}/yr", drop=True) # adjust units
+ceds = pix.units.convert_unit(ceds, lambda x: "Mt " + x.removeprefix("kt").strip()) # adjust units
 
-ceds = ceds.groupby(["em", "country", "unit", "sector"]).sum().pix.fixna()  # group and fix NAs
+ceds = ceds.groupby(["em", "country", "unit", "sector"]).sum().pix.fixna() # group and fix NAs
 
 # aggregate countries where this is necessary, e.g. because of specific other data (like SSP socioeconomic driver data)
 # TODO: check this based on the new SSP data (because old SSP data only had the sum); so recheck.
-country_combinations = {"isr_pse": ["isr", "pse"], "sdn_ssd": ["ssd", "sdn"], "srb_ksv": ["srb", "srb (kosovo)"]}
+country_combinations = {"isr_pse": ["isr", "pse"],
+                        "sdn_ssd": ["ssd", "sdn"],
+                        "srb_ksv": ["srb", "srb (kosovo)"]}
 ceds = ceds.pix.aggregate(country=country_combinations)
 
 # add global
@@ -100,9 +109,17 @@ ceds.pix.unique(unit_wishes.names).symmetric_difference(unit_wishes)
 
 # +
 # reformat
-ceds_ref = ceds.droplevel("unit").pix.semijoin(unit_wishes, how="left").rename_axis(index={"em": "gas"})
+ceds_ref = (
+    ceds
+    .droplevel("unit")
+    .pix.semijoin(unit_wishes, how="left")
+    .rename_axis(index={"em": "gas"})
+)
 
 
 # -
 
-(ceds_ref.to_csv(ceds_processed_output_file))
+(
+    ceds_ref
+    .to_csv(ceds_processed_output_file)
+)
