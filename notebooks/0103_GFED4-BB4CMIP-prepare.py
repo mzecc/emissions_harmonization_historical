a4fbfaccd776c1154522e36542ca499ca994512a
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-01-08 19:17:41 +0100

    Add first step of BB4CMIP processing
    Co-authored-by: chrisroadmap <chrisroadmap@gmail.com>

--- notebooks/0103_GFED4-BB4CMIP-prepare.py
+++ notebooks/0103_GFED4-BB4CMIP-prepare.py
@@ -14,25 +14,28 @@
 
 # %%
 from collections import defaultdict
+from pathlib import Path
 
-import cf_xarray.units
-import matplotlib.pyplot as plt
 import numpy as np
-import pint_xarray
+import ptolemy
 import scmdata
 import xarray as xr
+import xarray_regrid  # noqa: F401
+from tqdm import tqdm
 
-from emissions_harmonization_historical.constants import DATA_ROOT
-
-# %%
-pint_xarray.accessors.default_registry = pint_xarray.setup_registry(cf_xarray.units.units)
+from emissions_harmonization_historical.constants import DATA_ROOT, GFED_PROCESSING_ID
 
 # %%
 raw_data_path = DATA_ROOT / "national/gfed-bb4cmip/data_raw/data"
 raw_data_path
 
 # %%
-rcmip_data_path = DATA_ROOT / "global/rcmip/data_raw/rcmip-emissions-annual-means-v5-1-0.csv"
+gfed_processed_output_dir = DATA_ROOT / Path("national", "gfed-bb4cmip", "processed")
+gfed_processed_output_dir
+
+# %%
+gfed_data_aux_folder = DATA_ROOT / Path("national", "gfed", "data_aux")
+gfed_isomask = Path(gfed_data_aux_folder, "iso_mask.nc")  # for aggregating to countries
 
 # %% [markdown]
 # Group raw data into variable groups which can be used for processing.
@@ -45,114 +48,68 @@ for file in bb4cmip_files:
     variable = file.name.split("_")[0]
     bb4cmip_file_groups[variable].append(file)
 
-bb4cmip_file_groups.keys()
-
-# %% [markdown]
-# An example of loading data.
+# %%
+species_data = {
+    "BC": {
+        "unit_label": "Mt BC / yr",
+        "filename_label": "BC",
+    },
+    "NMVOC": {
+        "unit_label": "Mt NMVOC / yr",
+        "filename_label": "NMVOC_bulk",
+    },
+    "CO": {
+        "unit_label": "Mt CO / yr",
+        "filename_label": "CO",
+    },
+    "CO2": {
+        "unit_label": "Mt CO2 / yr",
+        "filename_label": "CO2",
+    },
+    "CH4": {
+        "unit_label": "Mt CH4 / yr",
+        "filename_label": "CH4",
+    },
+    "N2O": {
+        "unit_label": "Mt N2O / yr",
+        "filename_label": "N2O",
+    },
+    "OC": {
+        "unit_label": "Mt OC / yr",
+        "filename_label": "OC",
+    },
+    "NH3": {
+        "unit_label": "Mt NH3 / yr",
+        "filename_label": "NH3",
+    },
+    "NOx": {
+        "unit_label": "Mt NO / yr",
+        "filename_label": "NOx",
+    },
+    "SO2": {
+        "unit_label": "Mt SO2 / yr",
+        "filename_label": "SO2",
+    },
+}
 
 # %%
-cell_area = xr.open_mfdataset(bb4cmip_file_groups["gridcellarea"])
-cell_area
+# cell areas of GFED map
+cell_area_gfed = xr.open_mfdataset(bb4cmip_file_groups["gridcellarea"]).rename({"latitude": "lat", "longitude": "lon"})
+# Get this into memory so it doesn't load every time
+cell_area_gfed = cell_area_gfed.compute()
+# not sure needed
+
+# get iso map at half degree
+idxr = xr.open_dataarray(gfed_isomask, chunks="auto")  # chunks={"iso": 1})
+# Get this into memory so it doesn't load every time
+idxr = idxr.compute()
 
 # %%
-gas = "BC"
-unit_target = "Mt / yr"
-unit_label = "Mt BC / yr"  # Have to be careful here, depends on the species
-rcmip_gas = "BC"
-
-# gas = "CO"
-# unit_target = "Mt / yr"
-# unit_label = "Mt CO / yr"
-# rcmip_gas = "CO"
-
-# # TODO: check. I don't think we used this last time so we may want to ignore this again.
-# gas = "CO2"
-# unit_target = "Mt / yr"
-# unit_label = "Mt CO2 / yr"
-# rcmip_gas = "CO2"
-
-# gas = "CH4"
-# unit_target = "Mt / yr"
-# unit_label = "Mt CH4 / yr"
-# rcmip_gas = "CH4"
-
-# # TODO: check. I don't think we used this last time.
-# gas = "N2O"
-# unit_target = "Mt / yr"
-# unit_label = "Mt N2O / yr"
-# rcmip_gas = "N2O"
-
-# gas = "OC"
-# unit_target = "Mt / yr"
-# unit_label = "Mt OC / yr"
-# rcmip_gas = "OC"
-
-# gas = "NH3"
-# unit_target = "Mt / yr"
-# unit_label = "Mt NH3 / yr"
-# rcmip_gas = "NH3"
-
-# This reveals the bug in NOx processing for RCMIP v0.5.1 data
-# first identified by Chris.
-# Long story short: NOx emissions were too low by ~50%.
-# In this round, let's report NOx emissions with units of Mt N / yr
-# to avoid the ambiguous Mt NOx / yr.
-gas = "NOx"
-unit_target = "Mt / yr"
-unit_label = "Mt NO / yr"  # File metadata has molecular weight of 30 so assume NO
-rcmip_gas = "NOx"
-
-# gas = "SO2"
-# unit_target = "Mt / yr"
-# unit_label = "Mt SO2 / yr"
-# rcmip_gas = "Sulfur"
-
-# %%
-gas_ds = xr.open_mfdataset(bb4cmip_file_groups[gas], combine_attrs="drop_conflicts")
-gas_ds
-
-# %% [markdown]
-# Do a unit-aware sum
-
-# %%
-emissions_da = gas_ds[gas].pint.quantify()
-cellarea_da = cell_area["gridcellarea"].pint.quantify(gridcellarea="m^2")
-global_sum_emissions = (emissions_da * cellarea_da).sum(["latitude", "longitude"]).compute()
-global_sum_emissions
-
-# %%
-global_sum_emissions.plot()
-
-# %%
-# Should really weight the mean by the length of the month.
-# One for the future.
-global_sum_emissions_annual_mean = global_sum_emissions.groupby("time.year").mean()
-
-# Check that the sum matches the data provider's check values
-for original_file in bb4cmip_file_groups[gas]:
-    orig_ds = xr.open_dataset(original_file)
-    timerange = original_file.stem.split("_")[-1].split("-")
-
-    for i, timestamp in enumerate(timerange):
-        year = int(timestamp[:4])
-        if i < 1:
-            compare_val = float(orig_ds.attrs["annual_total_first_year_Tg_yr"])
-        elif i == 1:
-            compare_val = float(orig_ds.attrs["annual_total_last_year_Tg_yr"])
-        else:
-            raise NotImplementedError()
-
-        np.testing.assert_allclose(
-            global_sum_emissions_annual_mean.sel(year=year).pint.to("Tg / yr").data.m,
-            compare_val,
-            rtol=5e-2,
-        )
-
-global_sum_emissions_annual_mean.plot()
+idxr.sel(iso="aus").plot()
 
 
 # %%
-def gfed_to_scmrun(in_da: xr.DataArray, *, unit_target: str, unit_label: str, region: str) -> scmdata.ScmRun:
+def gfed_to_scmrun(in_da: xr.DataArray, *, unit_label: str, world: bool = False) -> scmdata.ScmRun:
     """
     Convert an `xr.DataArray` to `scmdata.ScmRun`
 
@@ -163,87 +120,88 @@ def gfed_to_scmrun(in_da: xr.DataArray, *, unit_target: str, unit_label: str, re
     in_da
         Input `xr.DataArray` to convert
 
-    unit_target
-        The unit to which to convert the data to.
-
-        This is done based on the original data unit,
-        so almost always has to exclude the gas.
-        I.e. this should be e.g. "Mt / yr", not "Mt BC / yr".
-
     unit_label
         The label to apply to the unit as it will be handled in the output.
 
-        This typically includes the gas, e.g. "Mt BC / yr"
+        This typically includes the species, e.g. "Mt BC / yr"
 
-    region
-        The region to which the data applies.
-
-        Typically "World".
+    world
+        Do global emissions (rather than national)
 
     Returns
     -------
     :
         `scmdata.ScmRun`
     """
-    df = in_da.pint.to(unit_target).pint.dequantify().to_pandas().to_frame().T
-    df["model"] = "GFED-BB4CMIP"
-    df["scenario"] = "historical-CMIP6Plus"
-    df["region"] = region
-    df["unit"] = unit_label
-    df["variable"] = f"Emissions|{gas}|BB4CMIP"
-    out = scmdata.ScmRun(df)
+    df = in_da.to_numpy()
+
+    if world:
+        region = "World"
+    else:
+        region = in_da.iso.to_numpy()
+
+    out = scmdata.ScmRun(
+        df,
+        columns=dict(
+            model="GFED-BB4CMIP",
+            scenario="historical",
+            region=region,
+            unit=unit_label,
+            variable=f"Emissions|{species}|BB4CMIP",
+        ),
+        index=in_da.year,
+    )
 
     return out
 
 
 # %%
-gfed_to_plot = gfed_to_scmrun(
-    global_sum_emissions_annual_mean, unit_target=unit_target, unit_label=unit_label, region="World"
-)
-gfed_to_plot.timeseries()
-
-# %%
-rcmip_data = scmdata.ScmRun(str(rcmip_data_path), lowercase_cols=True)
-rcmip_data
-
-# %%
-tmp = rcmip_data.filter(
-    variable=[
-        f"Emissions|{rcmip_gas}",
-        f"Emissions|{rcmip_gas}|MAGICC AFOLU",
-        f"Emissions|{rcmip_gas}|MAGICC AFOLU|*Burning*",
-    ],
-    region="World",
-    scenario=["historical", "ssp*"],
-)
-
-tmp_total = tmp.filter(variable=[f"Emissions|{rcmip_gas}", f"Emissions|{rcmip_gas}|MAGICC AFOLU"])
-tmp_total_burning = tmp.filter(variable=f"Emissions|{rcmip_gas}|MAGICC AFOLU|*Burning*", log_if_empty=False)
-if tmp_total_burning.empty:
-    # No RCMIP burning emissions
-    tmp = scmdata.run_append([tmp_total])
-
-else:
-    tmp_total_burning = tmp_total_burning.process_over(
-        "variable", "sum", op_cols={"variable": f"Emissions|{rcmip_gas}|MAGICC AFOLU|Burning"}, as_run=True, min_count=1
+gfed_processed_output_dir.mkdir(exist_ok=True, parents=True)
+for species in tqdm(species_data):
+    species_ds = xr.open_mfdataset(bb4cmip_file_groups[species], combine_attrs="drop_conflicts").rename(
+        {"latitude": "lat", "longitude": "lon"}
     )
-    tmp = scmdata.run_append([tmp_total, tmp_total_burning])
+    # Handy for testing
+    # species_ds = species_ds.isel(time=range(12 * 3))
+    # Handy for seeing what's going on
+    # display(species_ds)
 
-rcmip_to_plot = scmdata.run_append(
-    [
-        tmp.filter(scenario="historical"),
-        tmp.filter(scenario="historical", keep=False).filter(year=range(2014, 3000 + 1)),
-    ]
-)
+    # First get the emissions on to an annual grid
+    emissions_da = species_ds[species].chunk("auto")
+    assert emissions_da.units == "kg m-2 s-1"
 
-# %%
-to_plot = scmdata.run_append([gfed_to_plot, rcmip_to_plot]).convert_unit(unit_label)
+    # weight by month length
+    seconds_per_month = (
+        np.tile([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31], len(emissions_da.time) // 12) * 24 * 60 * 60
+    )
+    weights = xr.DataArray(seconds_per_month, dims=("time",), coords=emissions_da.coords["time"].coords)
 
-ax = to_plot.filter(scenario="hist*").lineplot(style="variable", alpha=0.5, linewidth=2)
-ax.legend(loc="center left", bbox_to_anchor=(1.05, 0.5))
-ax.set_ylim(ymin=0)
-plt.show()
+    # get annual sum emissions in each grid cell
+    gridded_annual_emissions_rate = (emissions_da * weights).groupby("time.year").sum().compute()  # kg / m2 / yr
 
-ax = to_plot.filter(variable=f"Emissions|{rcmip_gas}", keep=False).lineplot(style="variable", alpha=0.5, linewidth=2)
-ax.legend(loc="center left", bbox_to_anchor=(1.05, 0.5))
-ax.set_ylim(ymin=0)
+    # regrid to half degree
+    regridded_annual_emissions_rate = gridded_annual_emissions_rate.regrid.conservative(
+        idxr
+    )  # still kg / m2 / yr, now coarser grid
+
+    # get cell areas of new grid
+    cell_area = xr.DataArray(
+        ptolemy.cell_area(lats=regridded_annual_emissions_rate.lat, lons=regridded_annual_emissions_rate.lon)
+    )  # m2
+
+    # emissions in each country, annual time series
+    # Mt / yr in each country, the 1e9 is kg to Mt
+    assert species_ds[species].units == "kg m-2 s-1"
+    country_emissions = ((regridded_annual_emissions_rate * cell_area * idxr).sum(["lat", "lon"])).compute() / 1e9
+    world_emissions = (regridded_annual_emissions_rate * cell_area).sum(["lat", "lon"]).compute() / 1e9
+
+    out_world = gfed_to_scmrun(world_emissions, unit_label=species_data[species]["unit_label"], world=True)
+    out_country = gfed_to_scmrun(country_emissions, unit_label=species_data[species]["unit_label"])
+
+    # write out temporary files to save RAM and process combined dataset later
+
+    gfed_temp_file_world = gfed_processed_output_dir / f"{species}_world_{GFED_PROCESSING_ID}.csv"
+    out_world.timeseries(time_axis="year").to_csv(gfed_temp_file_world)
+    gfed_temp_file_country = gfed_processed_output_dir / f"{species}_national_{GFED_PROCESSING_ID}.csv"
+    out_country.timeseries(time_axis="year").to_csv(gfed_temp_file_country)
+    break
