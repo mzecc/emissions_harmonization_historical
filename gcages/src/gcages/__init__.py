0b2de40339411d357e1f80241561f88b66d947b7
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-01-23 14:45:49 +0100

    Initialise package

---
+++ gcages/src/gcages/__init__.py
@@ -0,0 +1,11 @@
+"""
+Core package for climate assessment workflows.
+
+Specifically, for workflows
+for assessing the global climate implications of global emission scenarios,
+typically from IAMs.
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("gcages")
