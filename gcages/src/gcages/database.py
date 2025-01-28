3588ba294c4b751aec756a90f6817a361b7c6e94
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-01-28 10:15:50 +0100

    Sort out partial overwrite during saving

--- gcages/src/gcages/database.py
+++ gcages/src/gcages/database.py
@@ -6,12 +6,40 @@ Super simple, mainly designed as a caching helper for simple climate model runs
 
 from __future__ import annotations
 
+import os
 from pathlib import Path
 
 import filelock
+import numpy as np
 import pandas as pd
 import pandas_indexing as pix
 from attrs import define
+
+
+class AlreadyInDBError(ValueError):
+    """
+    Raised when saving data would overwrite data which is already in the database
+    """
+
+    def __init__(
+        self, data: pd.DataFrame, already_in_db: np.typing.NDArray[np.bool]
+    ) -> None:
+        """
+        Initialise the error
+
+        Parameters
+        ----------
+        data
+            Data that we are trying to save
+
+        already_in_db
+            Rows that are already in the database
+        """
+        error_msg = (
+            "The following rows are already in the database:\n"
+            f"{data.index[already_in_db]}"
+        )
+        super().__init__(error_msg)
 
 
 @define
@@ -74,7 +102,12 @@ class GCDB:
         res = index.droplevel("filepath")
         return res
 
-    def save(self, data: pd.DataFrame, lock_acquire_timeout: int = 10.0) -> None:
+    def save(
+        self,
+        data: pd.DataFrame,
+        lock_acquire_timeout: int = 10.0,
+        allow_overwrite: bool = False,
+    ) -> None:
         # Save entire frame into single file.
         # If user wants things broken up,
         # they should do that before calling `save`.
@@ -85,16 +118,39 @@ class GCDB:
                     index_existing.drop("filepath", axis="columns")
                 )
                 already_in_db = data.index.isin(metadata_existing)
-                if already_in_db.any():
-                    raise NotImplementedError
+                if already_in_db.any() and not allow_overwrite:
+                    raise AlreadyInDBError(data=data, already_in_db=already_in_db)
 
-                data_file_path = self.get_new_data_file_path(
-                    file_id=len(index_existing["filepath"].unique())
-                )
+                file_id = len(index_existing["filepath"].unique())
+                data_file_path = self.get_new_data_file_path(file_id=file_id)
                 index_data = data.index.to_frame(index=False)
                 index_data["filepath"] = data_file_path
 
-                index = pd.concat([index_existing, index_data])
+                if already_in_db.any():
+                    if not allow_overwrite:  # pragma: no cover
+                        msg = "Should have already raised above"
+                        raise AssertionError(msg)
+
+                    index_existing_keep = _update_index_for_overwrite(
+                        db=self,
+                        filepaths_existing=index_existing.set_index(
+                            metadata_existing.names
+                        )["filepath"],
+                        data_to_write=data,
+                        already_in_db=already_in_db,
+                        file_id_base=file_id,
+                    )
+
+                    files_to_remove = set(index_existing["filepath"]).difference(
+                        set(index_existing_keep["filepath"])
+                    )
+                    for ftr in files_to_remove:
+                        os.remove(ftr)
+
+                    index = pd.concat([index_existing_keep, index_data])
+
+                else:
+                    index = pd.concat([index_existing, index_data])
 
             else:
                 data_file_path = self.get_new_data_file_path(file_id=0)
@@ -103,3 +159,35 @@ class GCDB:
 
             index.to_csv(self.index_file, index=False)
             data.to_csv(data_file_path)
+
+
+def _update_index_for_overwrite(
+    db: GCDB,
+    filepaths_existing: pd.Series[Path],
+    data_to_write: pd.DataFrame,
+    already_in_db: np.typing.NDArray[np.bool],
+    file_id_base: int,
+) -> pd.MultiIndex:
+    remove_loc = filepaths_existing.index.isin(data_to_write.index[already_in_db])
+
+    filepaths_remove = set(filepaths_existing[remove_loc])
+    filepaths_keep = set(filepaths_existing[~remove_loc])
+    filepaths_overlap = filepaths_remove.intersection(filepaths_keep)
+
+    filepaths_out = filepaths_existing.copy()
+    if filepaths_overlap:
+        for i, file in enumerate(filepaths_overlap):
+            overlap_file_data = pd.read_csv(file).set_index(
+                filepaths_existing.index.names
+            )
+            overlap_idxs = overlap_file_data.index.isin(data_to_write.index)
+            non_overlap_data = overlap_file_data[~overlap_idxs]
+            non_overlap_data_file_path = db.get_new_data_file_path(
+                file_id=file_id_base + i + 1
+            )
+            non_overlap_data.to_csv(non_overlap_data_file_path)
+            filepaths_out.loc[~overlap_idxs] = non_overlap_data_file_path
+
+    index_out = filepaths_out[~remove_loc].reset_index()
+
+    return index_out
--- gcages/tests/integration/test_database.py
+++ gcages/tests/integration/test_database.py
@@ -5,6 +5,9 @@ Tests of our database (`gcages.database`)
 from __future__ import annotations
 
 import itertools
+import re
+from functools import partial
+from pathlib import Path
 
 import filelock
 import numpy as np
@@ -12,7 +15,7 @@ import pandas as pd
 import pandas_indexing as pix
 import pytest
 
-from gcages.database import GCDB
+from gcages.database import GCDB, AlreadyInDBError
 
 
 def create_test_df(
@@ -112,6 +115,151 @@ def test_save_multiple_and_load(tmpdir):
     pd.testing.assert_frame_equal(all_saved, loaded)
 
 
+def test_save_overwrite_error(tmpdir):
+    db = GCDB(tmpdir)
+
+    cdf = partial(
+        create_test_df,
+        n_scenarios=10,
+        n_variables=1,
+        n_runs=3,
+        timepoints=np.array([2010.0, 2020.0, 2025.0, 2030.0]),
+    )
+
+    dup = cdf(units="m")
+    db.save(dup)
+
+    to_save = pix.concat([dup, cdf(units="km")])
+
+    error_msg = re.escape(
+        f"The following rows are already in the database:\n{dup.index}"
+    )
+    with pytest.raises(AlreadyInDBError, match=error_msg):
+        db.save(to_save)
+
+
+def test_save_overwrite_force(tmpdir):
+    db = GCDB(Path(tmpdir))
+
+    cdf = partial(
+        create_test_df,
+        n_scenarios=10,
+        n_variables=1,
+        n_runs=3,
+        timepoints=np.array([2010.0, 2020.0, 2025.0, 2030.0]),
+    )
+
+    original = cdf(units="m")
+    db.save(original)
+
+    # Make sure that our data saved correctly
+    db_metadata = db.load_metadata()
+    metadata_compare = db_metadata
+    pd.testing.assert_index_equal(
+        original.index, metadata_compare, exact="equiv", check_order=False
+    )
+
+    loaded = db.load()
+
+    pd.testing.assert_frame_equal(original, loaded)
+
+    original_overwrite = cdf(units="m")
+    updated = pix.concat([original_overwrite, cdf(units="km")])
+
+    # With force, we can overwrite
+    db.save(updated, allow_overwrite=True)
+
+    # As a helper, check we've got the number of files we expect.
+    # This is testing implementation, so could be removed in future.
+    # Expect to have the index file plus the new file, but not the original file.
+    db_files = list(db.db_dir.glob("*.csv"))
+    assert set([f.name for f in db_files]) == {"1.csv", "index.csv"}
+
+    # Check that the data was overwritten with new data
+    try:
+        pd.testing.assert_frame_equal(original, original_overwrite)
+    except AssertionError:
+        pass
+    else:
+        # Somehow got the same DataFrame,
+        # so there won't be any difference in the db.
+        msg = "Test won't do anything"
+        raise AssertionError(msg)
+
+    db_metadata = db.load_metadata()
+    metadata_compare = db_metadata
+    pd.testing.assert_index_equal(
+        updated.index, metadata_compare, exact="equiv", check_order=False
+    )
+
+    loaded = db.load()
+
+    pd.testing.assert_frame_equal(updated, loaded)
+
+
+def test_save_overwrite_force_half_overlap(tmpdir):
+    db = GCDB(Path(tmpdir))
+
+    cdf = partial(
+        create_test_df,
+        n_variables=5,
+        n_runs=3,
+        timepoints=np.array([2010.0, 2020.0, 2025.0, 2030.0]),
+        units="m",
+    )
+
+    original = cdf(n_scenarios=6)
+    db.save(original)
+
+    # Make sure that our data saved correctly
+    db_metadata = db.load_metadata()
+    metadata_compare = db_metadata
+    pd.testing.assert_index_equal(
+        original.index, metadata_compare, exact="equiv", check_order=False
+    )
+
+    loaded = db.load()
+
+    pd.testing.assert_frame_equal(original, loaded)
+
+    original_overwrite = cdf(n_scenarios=3)
+
+    # With force, we can overwrite
+    db.save(original_overwrite, allow_overwrite=True)
+
+    # As a helper, check we've got the number of files we expect.
+    # This is testing implementation, so could be removed in future.
+    # Expect to have the index file plus the new file plus the newly written file
+    # (to handle the need to split the original data so we can keep only what we need),
+    # but not the original file.
+    db_files = list(db.db_dir.glob("*.csv"))
+    assert set([f.name for f in db_files]) == {"1.csv", "2.csv", "index.csv"}
+
+    # Check that the data was overwritten with new data
+    overlap_idx = original.index.isin(original_overwrite.index)
+    overlap = original.loc[overlap_idx]
+    try:
+        pd.testing.assert_frame_equal(overlap, original_overwrite)
+    except AssertionError:
+        pass
+    else:
+        # Somehow got the same values,
+        # so there won't be any difference in the db.
+        msg = "Test won't do anything"
+        raise AssertionError(msg)
+
+    update_exp = pix.concat([original.loc[~overlap_idx], original_overwrite])
+    db_metadata = db.load_metadata()
+    metadata_compare = db_metadata
+    pd.testing.assert_index_equal(
+        update_exp.index, metadata_compare, exact="equiv", check_order=False
+    )
+
+    loaded = db.load()
+
+    pd.testing.assert_frame_equal(update_exp, loaded)
+
+
 def test_locking_load(tmpdir):
     db = GCDB(tmpdir)
 
@@ -173,7 +321,6 @@ def test_locking_save(tmpdir):
             db.save("not_used", lock_acquire_timeout=0.1)
 
 
-# - test overwrite-saving
 # - test partial loading
 # - test metadata loading
 # - test checking what is in what
