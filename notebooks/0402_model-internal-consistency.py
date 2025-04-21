03d26bdfec168e5b79311bee5ab11c48334edd83
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-04-21 18:28:02 +0200

    Add extra outputs when checking internal consistency

--- notebooks/0402_model-internal-consistency.py
+++ notebooks/0402_model-internal-consistency.py
@@ -21,6 +21,9 @@
 # ## Imports
 
 # %%
+import textwrap
+from collections import defaultdict
+from collections.abc import Iterable
 from functools import partial
 from pathlib import Path
 
@@ -31,6 +34,11 @@ import pandas_openscm
 import tqdm.auto as tqdm
 from gcages.cmip7_scenariomip.pre_processing import (
     get_independent_index_input,
+)
+from gcages.cmip7_scenariomip.pre_processing.constants import (
+    OPTIONAL_MODEL_REGION_VARIABLES_INPUT,
+    REQUIRED_MODEL_REGION_VARIABLES_INPUT,
+    REQUIRED_WORLD_VARIABLES_INPUT,
 )
 from gcages.index_manipulation import combine_species, split_sectors
 from gcages.testing import compare_close
@@ -70,6 +78,9 @@ SCENARIO_PATH = DATA_ROOT / "scenarios" / "data_raw"
 SCENARIO_PATH
 
 # %%
+level_separator = "|"
+
+# %%
 SCENARIO_DB = OpenSCMDB(
     db_dir=SCENARIO_PATH / SCENARIO_TIME_ID / "db",
     backend_data=FeatherDataBackend(),
@@ -96,6 +107,125 @@ def set_cell(
     cell.value = value
     if font is not None:
         cell.font = font
+
+
+# %%
+def group_into_levels(v_list: Iterable[str], level_separator: str = "|") -> dict[int, list[str]]:
+    """Group variables into common levels"""
+    n_sector_groupings = defaultdict(list)
+    for v in v_list:
+        n_sectors = v.count(level_separator) + 1
+        n_sector_groupings[n_sectors].append(v)
+
+    return dict(n_sector_groupings)
+
+
+def get_higher_lower_detail(level: int, ind: dict[int, str]) -> tuple[list[str], list[str]]:
+    """Get variables at higher and lower levels of detail than a given level"""
+    higher_detail = []
+    lower_detail = []
+    for k, v in ind.items():
+        if k > level:
+            higher_detail.extend(v)
+        elif k < level:
+            lower_detail.extend(v)
+
+    return higher_detail, lower_detail
+
+
+# %%
+def get_extras_l(
+    inconsistent_variable: str,
+    verbosity: int,
+    model_df: pd.DataFrame,
+    not_relevant: tuple[str, ...] = ("Emissions|CO2|AFOLU [NGHGI]",),
+) -> list[str]:
+    """
+    Get list of extra variables
+
+    In other words, variables that are reported
+    but aren't used and we can't see a clear reason
+    why this wouldn't be an issue.
+    """
+    not_relevant_s = set(not_relevant)
+
+    required_variables_grouped = group_into_levels(
+        v for v in REQUIRED_MODEL_REGION_VARIABLES_INPUT if f"{inconsistent_variable}{level_separator}" in v
+    )
+    optional_variables_grouped = group_into_levels(
+        v for v in OPTIONAL_MODEL_REGION_VARIABLES_INPUT if f"{inconsistent_variable}{level_separator}" in v
+    )
+
+    extras_l = []
+    for n_levels in sorted(required_variables_grouped.keys()):
+        exp_variables = required_variables_grouped[n_levels]
+
+        if n_levels in optional_variables_grouped:
+            opt_variables = optional_variables_grouped[n_levels]
+        else:
+            opt_variables = []
+
+        higher_detail_required, lower_detail_required = get_higher_lower_detail(n_levels, required_variables_grouped)
+        higher_detail_optional, lower_detail_optional = get_higher_lower_detail(n_levels, optional_variables_grouped)
+        higher_detail = [*higher_detail_required, *higher_detail_optional]
+        lower_detail = [*lower_detail_required, *lower_detail_optional]
+
+        exp_variables_s = set(exp_variables)
+        optional_variables_s = set(opt_variables)
+        reported_variables = set(
+            model_df.loc[
+                pix.ismatch(
+                    variable=level_separator.join(
+                        (f"{inconsistent_variable}{level_separator}*", *(["*"] * (n_levels - 3)))
+                    )
+                )
+            ].pix.unique("variable")
+        )
+
+        not_exp = reported_variables - exp_variables_s - optional_variables_s
+
+        reported_at_higher_detail = {v for v in not_exp if any(v in hd for hd in higher_detail)}
+        reported_at_lower_detail = {v for v in not_exp if any(ld in v for ld in lower_detail)}
+
+        reported_at_world_level = not_exp.intersection(
+            {
+                *REQUIRED_WORLD_VARIABLES_INPUT,
+                # No idea how to express the Bunkers exception well
+                level_separator.join([*inconsistent_variable.split(level_separator), "Energy", "Demand", "Bunkers"]),
+            }
+        )
+        extras = (
+            not_exp - reported_at_higher_detail - reported_at_lower_detail - reported_at_world_level - not_relevant_s
+        )
+
+        if verbosity > 0:
+            print(f"Level {n_levels - 2}")
+            print("=======")
+
+        def wrap_h(vs: set[str]) -> None:
+            return textwrap.indent("\n".join(sorted(vs)), prefix="  - ")
+
+        if verbosity > 1:
+            print(f"Reported:\n{wrap_h(exp_variables_s.intersection(reported_variables))}")
+            print(f"Reported optional:\n{wrap_h(optional_variables_s.intersection(reported_variables))}")
+
+        if verbosity > 0:
+            print(f"Missing:\n\n{wrap_h(exp_variables_s - reported_variables)}\n")
+
+        if verbosity > 1:
+            print(f"Missing optional:\n{wrap_h(optional_variables_s - reported_variables)}")
+            print(f"Reported at higher detail:\n{wrap_h(reported_at_higher_detail)}")
+            print(f"Reported at lower detail:\n{wrap_h(reported_at_lower_detail)}")
+            print(f"Reported at world level:\n{wrap_h(reported_at_world_level)}")
+
+        if verbosity > 0:
+            if extras:
+                print(f"Extras (these could be the cause of internal inconsistencies):\n\n{wrap_h(extras)}\n")
+                print("")
+
+        extras_l.extend(extras)
+
+    return extras_l
 
 
 # %% [markdown]
@@ -198,13 +328,10 @@ if not inconsistencies_l:
 else:
     print("Internal consistency issues were found")
     inconsistencies = pix.concat(inconsistencies_l)
-    # display(inconsistencies)
-    # TODO: save this out by variable
-    # inconsistencies.to_csv(output_dir_model / "internal-inconsistencies.csv", index=True)
-    # also include the components we considered
-    # also see if we can figure out whether there is anything obvious missing
-    # model_df_considered.loc[pix.ismatch(variable=f"{variable}**")]
-    # variable.split("|")[-1]
+
+# %%
+verbose_missing_reporting = "v"
+verbosity = verbose_missing_reporting.count("v")
 
 # %%
 if inconsistencies_l:
@@ -217,7 +344,12 @@ if inconsistencies_l:
         for inconsistent_variable, inconsistent_variable_df in tqdm.tqdm(
             inconsistencies.groupby("variable"), desc="variables"
         ):
-            species = inconsistent_variable.split("|")[-1]
+            inconsistent_variable_df.columns.name = "source"
+            inconsistent_variable_df_stacked = (
+                inconsistent_variable_df.unstack().stack("source", future_stack=True).sort_index()
+            )
+
+            species = inconsistent_variable.split(level_separator)[-1]
             species_worksheet = workbook.create_sheet(title=species)
 
             set_cell_ws = partial(set_cell, ws=species_worksheet)
@@ -244,15 +376,68 @@ if inconsistencies_l:
                 row=6,
                 col=1,
             )
-
-            inconsistent_variable_df.columns.name = "source"
-            inconsistent_variable_df_stacked = (
-                inconsistent_variable_df.unstack().stack("source", future_stack=True).sort_index()
+            set_cell_ws(
+                f"The scenarios were downloaded from the database at {SCENARIO_TIME_ID}",
+                row=7,
+                col=1,
             )
+
+            current_row = 7
+            extras_l = get_extras_l(inconsistent_variable=inconsistent_variable, verbosity=verbosity, model_df=model_df)
+            if extras_l:
+                extras_df = model_df.loc[pix.isin(variable=extras_l, region="World")]
+                totals_extras = combine_species(
+                    split_sectors(extras_df).openscm.groupby_except(["region", "sectors"]).sum()
+                ).pix.assign(region="World")
+
+                compare_incl_missing = compare_close(
+                    inconsistent_variable_df_stacked.loc[pix.isin(source="reported_total")].reset_index(
+                        "source", drop=True
+                    ),
+                    inconsistent_variable_df_stacked.loc[pix.isin(source="derived_from_input")]
+                    .add(totals_extras)
+                    .reset_index("source", drop=True),
+                    left_name="reported_total",
+                    right_name="derived_from_input_incl_extras",
+                )
+
+                current_row += 2
+                if compare_incl_missing.empty:
+                    set_cell_ws(
+                        (
+                            "The following variables are reported but not picked up in the hierarchy. "
+                            "Including them in the totals fixes the mismatch, "
+                            "so they likely just need to be reclassified/renamed."
+                        ),
+                        row=current_row,
+                        col=1,
+                        font=Font(bold=True),
+                    )
+
+                else:
+                    set_cell_ws(
+                        (
+                            "The following variables are reported but not picked up in the hierarchy. "
+                            "They may be the source of the inconsistency."
+                        ),
+                        row=current_row,
+                        col=1,
+                        font=Font(bold=True),
+                    )
+
+                for extra in extras_l:
+                    current_row += 1
+                    set_cell_ws(
+                        f"- {extra}",
+                        row=current_row,
+                        col=1,
+                    )
+
+            current_row += 2
             inconsistent_variable_df_stacked.to_excel(
                 writer,
                 sheet_name=species,
-                startrow=8 - 1,  # pandas offset by one
+                startrow=current_row - 1,  # pandas offset by one
             )
 
             components_included_in_sum = model_df_considered.loc[pix.ismatch(variable=f"{inconsistent_variable}**")]
@@ -262,19 +447,17 @@ if inconsistencies_l:
                 .sort_index()
             )
 
-            current_row = 8 - 1 + 1 + inconsistent_variable_df_stacked.shape[0]
+            current_row = current_row + inconsistent_variable_df_stacked.shape[0] + 2
             set_cell_ws(
                 "The following rows were used to create the sector-region sum",
-                row=current_row + 2,
+                row=current_row,
                 col=1,
                 font=Font(bold=True),
             )
             components_included_in_sum.to_excel(
                 writer,
                 sheet_name=species,
-                startrow=current_row + 4 - 1,  # pandas offset by one
+                startrow=current_row + 2 - 1,  # pandas offset by one
             )
-
-            # Possible extension: try and guess what variables were missing or double counted
 
         print(f"Wrote {internal_consistency_issues_filepath}")
