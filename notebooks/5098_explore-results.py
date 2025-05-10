45f25b295c02f9b9b4bc00f12836f4d8ab6e5af7
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-05-10 18:52:22 +0200

    After full run through

--- notebooks/5098_explore-results.py
+++ notebooks/5098_explore-results.py
@@ -21,6 +21,8 @@
 # ## Imports
 
 # %%
+import multiprocessing
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -68,13 +70,19 @@ temperatures_in_line_with_assessment = POST_PROCESSED_TIMESERIES_RUN_ID_DB.load(
 
 # %%
 raw_scm_output = SCM_OUTPUT_DB.load(
-    pix.ismatch(variable=[
-        "Heat Uptake",
-        "Effective Radiative Forcing|Aerosols",
-        "Effective Radiative Forcing|CO2",
-        "Effective Radiative Forcing|Greenhouse Gases",
-        "Effective Radiative Forcing|Ozone",
-    ])
+    pix.ismatch(
+        variable=[
+            "Heat Uptake",
+            "Effective Radiative Forcing|Aerosols",
+            "Effective Radiative Forcing|CO2",
+            "Effective Radiative Forcing|Greenhouse Gases",
+            "Effective Radiative Forcing|Ozone",
+            "Atmospheric Concentrations|CO2",
+            "Atmospheric Concentrations|CH4",
+        ]
+    ),
+    progress=True,
+    max_workers=multiprocessing.cpu_count(),
 )
 
 # %% [markdown]
@@ -108,16 +116,16 @@ tmp = (
 )
 
 tmp.loc[
-    :, :
-    # (tmp[("max", "K", 0.33)] > 0.0) & (tmp[("max", "K", 0.33)] < 1.6)
+    # :, :
+    (tmp[("max", "K", 0.33)] > 0.0) & (tmp[("max", "K", 0.33)] < 1.6)
+    # (tmp[("max", "K", 0.5)] > 1.7) & (tmp[("max", "K", 0.5)] < 1.9)
     # (tmp[("max", "K", 0.67)] > 1.8) & (tmp[("max", "K", 0.67)] < 2.05)
     # (tmp[("2100", "K", 0.5)] > 1.7)
     # & (tmp[("2100", "K", 0.5)] < 2.0)
-    # (tmp[("2100", "K", 0.5)] > 2.1)
-    # & (tmp[("2100", "K", 0.5)] < 2.5)
+    # (tmp[("2100", "K", 0.5)] > 2.2) & (tmp[("2100", "K", 0.5)] < 2.7)
     # # (tmp[("2100", "K", 0.5)] > 2.5)
     # # & (tmp[("2100", "K", 0.5)] < 3.0)
-    # # (tmp[("2100", "K", 0.5)] > 3.0) & (tmp[("2100", "K", 0.5)] < 3.6)
+    # (tmp[("2100", "K", 0.5)] > 3.0) & (tmp[("2100", "K", 0.5)] < 30.6)
 ]
 
 # %%
@@ -146,60 +154,96 @@ tmp
 
 # %%
 scratch_selection_l = [
-    # ("#7f3e3e", ("REMIND-MAgPIE 3.5-4.10", "SSP3 - High Emissions")),
-    # ("#7f3e3e", ("GCAM 7.1 scenarioMIP", "SSP5 - High Emissions")),
-    # ("#7f3e3e", ("IMAGE 3.4", "SSP5 - High Emissions")),
-    ("#7f3e3e", ("AIM 3.0", "SSP5 - High Emissions")),
-    # ("#f7a84f", ("REMIND-MAgPIE 3.5-4.10", "SSP2 - Medium Emissions")),
-    # ("#f7a84f", ("GCAM 7.1 scenarioMIP", "SSP2 - Medium Emissions")),
-    # ("#f7a84f", ("IMAGE 3.4", "SSP2 - Medium Emissions")),
-    # ("#f7a84f", ("MESSAGEix-GLOBIOM-GAINS 2.1-M-R12", "SSP2 - Medium Emissions")),
-    ("#f7a84f", ("WITCH 6.0", "SSP2 - Medium Emissions")),
-    # ("#e1ad01", ("REMIND-MAgPIE 3.5-4.10", "SSP3 - Medium-Low Emissions")),
+    # # H
+    # # ("#7f3e3e", ("REMIND-MAgPIE 3.5-4.10", "SSP3 - High Emissions")),
+    # # ("#7f3e3e", ("GCAM 7.1 scenarioMIP", "SSP5 - High Emissions")),
+    ("#7f3e3e", ("IMAGE 3.4", "SSP3 - High Emissions")),
+    # ("#7f3e3e", ("AIM 3.0", "SSP5 - High Emissions")),
+    # # M
+    # # ("#f7a84f", ("REMIND-MAgPIE 3.5-4.10", "SSP2 - Medium Emissions")),
+    ("#f7a84f", ("GCAM 7.1 scenarioMIP", "SSP2 - Medium Emissions")),
+    # # ("#f7a84f", ("IMAGE 3.4", "SSP2 - Medium Emissions")),
+    # # ("#f7a84f", ("MESSAGEix-GLOBIOM-GAINS 2.1-M-R12", "SSP2 - Medium Emissions")),
+    # ("#f7a84f", ("WITCH 6.0", "SSP2 - Medium Emissions")),
+    # # ML
+    # # ("#e1ad01", ("REMIND-MAgPIE 3.5-4.10", "SSP3 - Medium-Low Emissions")),
+    # # # Very high sulfur emissions, not ideal
     # ("#e1ad01", ("COFFEE 1.6", "SSP2 - Medium-Low Emissions")),
-    ("#e1ad01", ("GCAM 7.1 scenarioMIP", "SSP2 - Medium-Low Emissions")),
-    # ("#586643", ("REMIND-MAgPIE 3.5-4.10", "SSP1 - Medium-Low Emissions")),
+    # ("#e1ad01", ("GCAM 7.1 scenarioMIP", "SSP2 - Medium-Low Emissions")),
+    # # ("#586643", ("REMIND-MAgPIE 3.5-4.10", "SSP1 - Medium-Low Emissions")),
+    # L
     # ("#2e9e68", ("REMIND-MAgPIE 3.5-4.10", "SSP2 - Low Emissions")),
     # ("#2e9e68", ("COFFEE 1.6", "SSP2 - Low Emissions")),
     ("#2e9e68", ("MESSAGEix-GLOBIOM-GAINS 2.1-M-R12", "SSP2 - Low Emissions")),
+    # VLHO
     ("#4b3d89", ("REMIND-MAgPIE 3.5-4.10", "SSP2 - Low Overshoot_d")),
-    # ("#4b3d89", ("GCAM 7.1 scenarioMIP", "SSP2 - Low Overshoot")),x
-    ("#899edb", ("COFFEE 1.6", "SSP2 - Very Low Emissions")),
-    ("#899edb", ("WITCH 6.0", "SSP1 - Low Overshoot")),
+    # ("#4b3d89", ("GCAM 7.1 scenarioMIP", "SSP1 - Low Overshoot")),
+    # ("#4b3d89", ("GCAM 7.1 scenarioMIP", "SSP2 - Low Overshoot")),
+    # VLLO
+    # Not sure if any of models can produce a 1.5C stable rather than overshoot
+    # ("#899edb", ("COFFEE 1.6", "SSP2 - Very Low Emissions")),
+    # Something weird happening with 2023 emissions,
+    # probably from interpolation I would guess.
+    # Hopefully they can fix
+    # ("#899edb", ("WITCH 6.0", "SSP1 - Low Overshoot")),
+    # VLLOD
     ("#499edb", ("AIM 3.0", "SSP1 - Very Low Emissions_a")),
     # ("#499edb", ("AIM 3.0", "SSP1 - Very Low Emissions")),
+    # ("#499edb", ("GCAM 7.1 scenarioMIP", "SSP1 - Very Low Emissions")),
+    # ("#499edb", ("REMIND-MAgPIE 3.5-4.10", "SSP1 - Very Low Emissions_c")),
 ]
+
+
+# %%
+def add_model_scenario_column(indf: pd.DataFrame, ms_separator: str, ms_level: str, copy: bool = True) -> pd.DataFrame:
+    """
+    Add a model-scenario column
+
+    TODO: push this to pandas-openscm as something like
+    `update_index_levels_multi_input`
+    that allows users to updated index levels
+    based on the value of multiple other index columns.
+    """
+    out = indf
+    if copy:
+        out = out.copy()
+
+    # Push ability to create a new level from multiple other levels into pandas-openscm
+    new_name = ms_level
+    new_level = (
+        indf.index.droplevel(out.index.names.difference(["model", "scenario"]))
+        .drop_duplicates()
+        .map(lambda x: ms_separator.join(x))
+    )
+
+    if new_level.shape[0] != indf.shape[0]:
+        dup_level = out.index.get_level_values("model") + ms_separator + out.index.get_level_values("scenario")
+        new_level = dup_level.unique()
+        new_codes = new_level.get_indexer(dup_level)
+    else:
+        new_codes = np.arange(new_level.shape[0])
+
+    out.index = pd.MultiIndex(
+        levels=[*out.index.levels, new_level],
+        codes=[*out.index.codes, new_codes],
+        names=[*out.index.names, new_name],
+    )
+
+    return out
+
 
 # %%
 ms_separator = " || "
-palette = {ms_separator.join(v[1]): v[0] for v in scratch_selection_l}
-scratch_selection = pd.MultiIndex.from_tuples([v[1] for v in scratch_selection_l], names=["model", "scenario"])
-pdf_temperature = temperatures_in_line_with_assessment.loc[pix.isin(climate_model="MAGICCv7.6.0a3"), 2000:].openscm.mi_loc(
-    scratch_selection
-)
-
 ms_level = "model || scenario"
+palette = {ms_separator.join(v[1]): v[0] for v in scratch_selection_l}
+palette_model = {k.split(ms_separator)[0]: v for k, v in palette.items()}
+scratch_selection = pd.MultiIndex.from_tuples([v[1] for v in scratch_selection_l], names=["model", "scenario"])
+pdf_temperature = temperatures_in_line_with_assessment.loc[
+    pix.isin(climate_model="MAGICCv7.6.0a3"), 2000:
+].openscm.mi_loc(scratch_selection)
 
-# Push ability to create a new level from multiple other levels into pandas-openscm
-new_name = ms_level
-new_level = (
-    pdf_temperature.index.droplevel(pdf_temperature.index.names.difference(["model", "scenario"]))
-    .drop_duplicates()
-    .map(lambda x: ms_separator.join(x))
-)
-
-if new_level.shape[0] != pdf_temperature.shape[0]:
-    dup_level = pdf_temperature.index.get_level_values("model") + " || " + pdf_temperature.index.get_level_values("scenario")
-    new_level = dup_level.unique()
-    new_codes = new_level.get_indexer(dup_level)
-else:
-    new_codes = np.arange(new_level.shape[0])
-
-pdf_temperature.index = pd.MultiIndex(
-    levels=[*pdf_temperature.index.levels, new_level],
-    codes=[*pdf_temperature.index.codes, new_codes],
-    names=[*pdf_temperature.index.names, new_name],
-)
+pdf_temperature = add_model_scenario_column(pdf_temperature, ms_separator=ms_separator, ms_level=ms_level)
+# pdf_temperature
 
 
 # %%
@@ -208,14 +252,20 @@ def create_legend(ax, handles) -> None:
     ax.legend(handles=handles, loc="center left", bbox_to_anchor=(1.05, 0.0))
 
 
+hue = ms_level
+palette_h = palette
+hue = "model"
+palette_h = palette_model
+
 fig, axes = plt.subplots(nrows=2, figsize=(4, 8), sharex=True)
 for i, (ax, yticks) in enumerate(zip(axes, [np.arange(0.5, 4.01, 0.5), np.arange(0.7, 2.21, 0.1)])):
     pdf_temperature.openscm.plot_plume_after_calculating_quantiles(
         quantile_over="run_id",
-        hue_var=ms_level,
+        hue_var=hue,
         style_var="climate_model",
-        palette=palette,
-        quantiles_plumes=((0.5, 1.0), ((0.33, 0.67), 0.3), ((0.05, 0.95), 0.0)),
+        palette=palette_h,
+        quantiles_plumes=((0.5, 1.0), ((0.33, 0.67), 0.75), ((0.05, 0.95), 0.0)),
+        # quantiles_plumes=((0.5, 1.0), ((0.33, 0.67), 0.0), ((0.05, 0.95), 0.0)),
         ax=ax,
         create_legend=(lambda x, y: None) if i > 0 else create_legend,
     )
@@ -226,29 +276,12 @@ for i, (ax, yticks) in enumerate(zip(axes, [np.arange(0.5, 4.01, 0.5), np.arange
     ax.grid()
 
 # %%
-scm_emissions = SCM_OUTPUT_DB.load(pix.ismatch(variable="Emissions|**"))
-# scm_emissions
-pdf_emissions = scm_emissions.copy()
-
-# Push ability to create a new level from multiple other levels into pandas-openscm
-new_name = ms_level
-new_level = (
-    pdf_emissions.index.droplevel(pdf_emissions.index.names.difference(["model", "scenario"]))
-    .drop_duplicates()
-    .map(lambda x: ms_separator.join(x))
-)
-
-if new_level.shape[0] != pdf_emissions.shape[0]:
-    dup_level = pdf_emissions.index.get_level_values("model") + " || " + pdf_emissions.index.get_level_values("scenario")
-    new_level = dup_level.unique()
-    new_codes = new_level.get_indexer(dup_level)
-else:
-    new_codes = np.arange(new_level.shape[0])
-
-pdf_emissions.index = pd.MultiIndex(
-    levels=[*pdf_emissions.index.levels, new_level],
-    codes=[*pdf_emissions.index.codes, new_codes],
-    names=[*pdf_emissions.index.names, new_name],
+pdf_emissions = add_model_scenario_column(
+    SCM_OUTPUT_DB.load(pix.ismatch(variable="Emissions|**", climate_model="MAGICCv7.6.0a3")).reset_index(
+        "climate_model", drop=True
+    ),
+    ms_separator=ms_separator,
+    ms_level=ms_level,
 )
 pdf_emissions = pdf_emissions.pix.format(variable="{variable} ({unit})", drop=True).openscm.mi_loc(scratch_selection)
 
@@ -283,13 +316,13 @@ for ax in fg.axes.flatten():
     ax.grid()
 
 # %%
-sorted(pdf_emissions.pix.unique("variable"))
+pdf_raw_scm_output = add_model_scenario_column(
+    raw_scm_output.loc[pix.isin(climate_model="MAGICCv7.6.0a3"), 2000:].openscm.mi_loc(scratch_selection),
+    ms_separator=ms_separator,
+    ms_level=ms_level,
+)
 
-# %%
-assert False, "Add to model-scenario function or similar"
-
-# %%
-raw_scm_output
+# pdf_raw_scm_output
 
 # %%
 variables_src = [
@@ -304,56 +337,74 @@ variables_src = [
     ("Emissions|NOx", pdf_emissions, True, False),
     ("Emissions|NH3", pdf_emissions, True, False),
     ("Emissions|VOC", pdf_emissions, True, False),
+    ("Atmospheric Concentrations|CO2", pdf_raw_scm_output, False, False),
+    ("Atmospheric Concentrations|CH4", pdf_raw_scm_output, False, False),
+    ("Effective Radiative Forcing|CO2", pdf_raw_scm_output, False, False),
+    ("Effective Radiative Forcing|Greenhouse Gases", pdf_raw_scm_output, False, False),
+    ("Effective Radiative Forcing|Aerosols", pdf_raw_scm_output, False, False),
+    ("Heat Uptake", pdf_raw_scm_output, False, False),
     ("Surface Temperature (GSAT)", pdf_temperature, False, True),
 ]
 
-fig, axes = plt.subplots(nrows=6, ncols=2, figsize=(12, 6 * 5))
+nrows = len(variables_src) // 2 + len(variables_src) % 2
+fig, axes = plt.subplots(nrows=nrows, ncols=2, figsize=(12, nrows * 5))
 axes_flat = axes.flatten()
+hue = ms_level
+palette_h = palette
+hue = "model"
+palette_h = palette_model
 
 for i, (variable, src, emissions, show_legend) in tqdm.auto.tqdm(enumerate(variables_src)):
     ax = axes_flat[i]
 
     if emissions:
-        vdf = pdf_emissions.loc[pix.ismatch(variable=f"{variable}*")].openscm.to_long_data()
-        
+        vdf = src.loc[pix.ismatch(variable=f"{variable}*")].openscm.to_long_data()
+
         sns.lineplot(
             ax=ax,
             data=vdf,
             x="time",
             y="value",
-            hue="model || scenario",
-            palette=palette,
+            hue=hue,
+            palette=palette_h,
         )
         ax.set_title(vdf["variable"].unique()[0], fontdict=dict(fontsize="medium"))
-        
+
     else:
-        src.loc[pix.isin(variable=variable)].openscm.plot_plume_after_calculating_quantiles(
+        vdf = src.loc[pix.isin(variable=variable)]
+        vdf.openscm.plot_plume_after_calculating_quantiles(
             quantile_over="run_id",
-            hue_var=ms_level,
+            hue_var=hue,
             style_var="climate_model",
-            palette=palette,
+            palette=palette_h,
             quantiles_plumes=((0.5, 1.0), ((0.33, 0.67), 0.3), ((0.05, 0.95), 0.0)),
             ax=ax,
         )
-        ax.set_title(src.pix.unique("variable")[0], fontdict=dict(fontsize="medium"))
-        
-    if show_legend:
+        ax.set_title(vdf.pix.unique("variable")[0], fontdict=dict(fontsize="medium"))
+
+    # if show_legend:
+    if i % 2:
         sns.move_legend(ax, loc="center left", bbox_to_anchor=(1.05, 0.5))
     else:
         ax.legend().remove()
-    
-    if "CO2" in variable:
+
+    if "Emissions" in variable and "CO2" in variable:
         ax.axhline(0.0, linestyle="--", color="gray")
         if "Energy" in variable:
             ax.set_yticks(np.arange(-1e4, 6e4 + 1, 1e4))
         else:
-            ax.set_yticks(np.arange(-5e3, 7e3 + 1, 1e3))
+            ax.set_yticks(np.arange(-7.5e3, 10e3 + 1, 2.5e3))
+
+    elif "Effective Radiative Forcing" in variable:
+        pass
 
     else:
         ax.set_ylim(ymin=0.0)
 
     ax.grid()
     # break
+
+plt.savefig("full-dive.pdf", bbox_inches="tight")
 
 # %%
 multi_index_lookup(categories, scratch_selection).unstack(["metric", "climate_model"]).sort_values(
@@ -383,7 +434,7 @@ multi_index_lookup(metadata_quantile, scratch_selection).unstack(["metric", "uni
 # ### How much difference is the MAGICC update making?
 
 # %%
-iam = "WITCH"
+iam = "COFFEE"
 tmp = temperatures_in_line_with_assessment.loc[pix.ismatch(model=f"{iam}**"), 2000:]
 
 fig, axes = plt.subplots(ncols=2, figsize=(16, 4))
