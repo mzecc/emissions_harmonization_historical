c4f58170f123f9dfda6a1c536b5a1c41ed9a64cd
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-05-12 10:37:56 +0200

    Add RCMIP unpacking

--- scripts/pack-unpack-scenario-running-data.py
+++ scripts/pack-unpack-scenario-running-data.py
@@ -17,6 +17,8 @@ from emissions_harmonization_historical.constants_5000 import (
     INFILLING_DB,
     INFILLING_DB_DIR,
     RAW_SCENARIO_DB,
+    RCMIP_PROCESSED_DB,
+    RCMIP_PROCESSING_ID,
     WMO_2022_PROCESSED_DB,
     WMO_2022_PROCESSING_ID,
 )
@@ -34,6 +36,7 @@ def main(pack: bool = True) -> None:
         infilling_db_data = INFILLING_DB.load()
         cmip_processed = CMIP7_GHG_PROCESSED_DB.load()
         wmo_processed = WMO_2022_PROCESSED_DB.load()
+        rcmip_processed = RCMIP_PROCESSED_DB.load()
 
         for data, gzip in (
             (raw_scenario_data, REPO_ROOT / f"raw-scenarios_{DOWNLOAD_SCENARIOS_ID}.tar.gz"),
@@ -41,6 +44,7 @@ def main(pack: bool = True) -> None:
             (infilling_db_data, REPO_ROOT / f"infilling-db_{INFILLING_DB_DIR.name}.tar.gz"),
             (cmip_processed, REPO_ROOT / f"cmip-processed_{CMIP7_GHG_PROCESSING_ID}.tar.gz"),
             (wmo_processed, REPO_ROOT / f"wmo-processed_{WMO_2022_PROCESSING_ID}.tar.gz"),
+            (rcmip_processed, REPO_ROOT / f"rcmip-processed_{RCMIP_PROCESSING_ID}.tar.gz"),
         ):
             tmp_dir = Path(tempfile.mkdtemp())
             tmp_db_dir = tmp_dir / "db"
@@ -56,17 +60,18 @@ def main(pack: bool = True) -> None:
 
     else:
         for gzip, dest in (
-            # (REPO_ROOT / f"raw-scenarios_{DOWNLOAD_SCENARIOS_ID}.tar.gz", RAW_SCENARIO_DB.db_dir),
-            # (
-            #     REPO_ROOT / f"harmonisation-history_{HISTORY_FOR_HARMONISATION_ID}.tar.gz",
-            #     HISTORY_HARMONISATION_DB.db_dir,
-            # ),
-            # (
-            #     REPO_ROOT / f"infilling-db_{INFILLING_DB_DIR.name}.tar.gz",
-            #     INFILLING_DB.db_dir,
-            # ),
+            (REPO_ROOT / f"raw-scenarios_{DOWNLOAD_SCENARIOS_ID}.tar.gz", RAW_SCENARIO_DB.db_dir),
+            (
+                REPO_ROOT / f"harmonisation-history_{HISTORY_FOR_HARMONISATION_ID}.tar.gz",
+                HISTORY_HARMONISATION_DB.db_dir,
+            ),
+            (
+                REPO_ROOT / f"infilling-db_{INFILLING_DB_DIR.name}.tar.gz",
+                INFILLING_DB.db_dir,
+            ),
             (REPO_ROOT / f"cmip-processed_{CMIP7_GHG_PROCESSING_ID}.tar.gz", CMIP7_GHG_PROCESSED_DB.db_dir),
             (REPO_ROOT / f"wmo-processed_{WMO_2022_PROCESSING_ID}.tar.gz", WMO_2022_PROCESSED_DB.db_dir),
+            (REPO_ROOT / f"rcmip-processed_{RCMIP_PROCESSING_ID}.tar.gz", RCMIP_PROCESSED_DB.db_dir),
         ):
             OpenSCMDB.from_gzipped_tar_archive(
                 tar_archive=gzip,
