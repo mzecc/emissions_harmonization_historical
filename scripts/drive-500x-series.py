b0b3843fa41c8d00afdf76319ee7326c220a733f
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-06-05 17:21:34 +0200

    Fix up plotting option handling

--- scripts/drive-500x-series.py
+++ scripts/drive-500x-series.py
@@ -36,9 +36,14 @@ def get_notebook_parameters(notebook_name: str, iam: str, scm: str | None = None
 
     elif notebook_name in [
         "5094_harmonisation.py",
+    ]:
+        res = {"model": iam, "make_region_sector_plots": True, "output_to_pdf": True}
+        res = {"model": iam, "make_region_sector_plots": False, "output_to_pdf": False}
+
+    elif notebook_name in [
         "5190_infilling.py",
     ]:
-        res = {"model": iam, "output_to_pdf": True}
+        res = {"model": iam}
 
     elif notebook_name in [
         "5191_run-simple-climate-model.py",
@@ -47,7 +52,6 @@ def get_notebook_parameters(notebook_name: str, iam: str, scm: str | None = None
         if scm is None:
             raise TypeError(scm)
 
-        # res = {"model": iam, "scm": scm, "n_processes" n_processes}
         res = {"model": iam, "scm": scm}
 
     else:
@@ -163,8 +167,7 @@ def main():  # noqa: PLR0912
         "COFFEE",
         "AIM",
     ]
-
-    # iams = ["IMAGE"]
+    # iams = ["COFFEE"]
     # Single notebook
     notebook_prefixes = ["5090"]
     # # Everything except downloads and reporting checking
